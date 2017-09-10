fecaea085e3e271a2942641e77e1bce9b96b08aa
Koichi Murakami <koichi.murakami@kek.jp>
2017-09-10 16:25:57 +0900

    change module structure

--- g4kernel/__init__.py
+++ g4kernel/__init__.py
@@ -8,3 +8,4 @@ This software is distributed WITHOUT ANY WARRANTY; without even the
 implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the License for more information.
 """
+
